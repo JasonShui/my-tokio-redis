91efddc6e4be14f22ab0a119e69f9c2ecb9699c1
JasonSui <jiashui@hotmail.com>
2021-01-02 21:31:36 +0800

    impl data store

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,46 @@
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+use mini_redis::Command::{self, Get, Set};
+use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+        tokio::spawn(async move {
+            process(socket).await;
+        });
     }
 }
 
 async fn process(socket: TcpStream) {
+    let mut db = HashMap::new();
+
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("got: {:?}", frame);
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                println!("recv set cmd {:?}", cmd);
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                println!("recv get cmd {:?}", cmd);
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => {
+                println!("not implemented cmd: {:?}", cmd);
+                Frame::Null
+            }
+        };
 
-        let response = Frame::Error("unimplemented".to_string());
+        println!("curr redis data: {:?}", db);
         connection.write_frame(&response).await.unwrap();
     }
 }
